75781c353066519f74194584becf6c57f660c441
Knot <knotto.vt@gmail.com>
2022-04-30 16:18:09 +0700

    add files

---
+++ CardAnimation/CardAnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  CardAnimationApp.swift
+//  CardAnimation
+//
+//  Created by Visarut Tippun on 30/4/22.
+//
+
+import SwiftUI
+
+@main
+struct CardAnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
